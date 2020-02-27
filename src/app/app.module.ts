d4025b166146a23732ce0ac9926ea1573b541a1c
Foodie <olaorefouad565@gmail.com>
2020-02-27 10:34:02 +0100

    design: finished designing the header and the search section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserItemComponent } from './views/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
